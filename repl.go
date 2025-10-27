42274f91dc3b2f27d8b2b1dbfe88fa4c3511047a
jkk290 <jared@kuromoto.dev>
2025-10-27 06:01:54 -1000

    Build initial repl function loop

--- repl.go
+++ repl.go
@@ -1,9 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	words := strings.Fields(text)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		cmdInput := scanner.Text()
+		cleanedInput := cleanInput(cmdInput)
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		fmt.Println("Your command was:", cleanedInput[0])
+	}
 }
